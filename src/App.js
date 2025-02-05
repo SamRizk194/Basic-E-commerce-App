14b98e0e99eb61858a5d3fd6bc3866871a32ad38
unknown <samehrizk1993@gmail.com>
2025-02-05 23:19:52 +0200

    adding about section and browser router dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import ProductsList from "./components/ProductsList";
 import Slider from "./components/Slider";
+import { Routes, Route } from "react-router";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Slider />
-      <ProductsList />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Slider />
+              <ProductsList />
+            </>
+          }
+        ></Route>
+        <Route path="about" element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
