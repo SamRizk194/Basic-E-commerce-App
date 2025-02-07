0994479054e24a61dc8620cfa88580c4fddea789
unknown <samehrizk1993@gmail.com>
2025-02-08 00:09:27 +0200

    adding categories

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -2,19 +2,59 @@ import { useState, useEffect } from "react";
 import Card from "./Card";
 
 function ProductsList() {
-  const [products, setProducts] = useState([]);
   const api_url = "https://fakestoreapi.com/products";
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
+  const getProducts = () => {
     fetch(api_url)
       .then((res) => res.json())
       .then((data) => setProducts(data));
+  };
+
+  const getCategories = () => {
+    fetch(`${api_url}/categories`)
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  };
+  const getProductInCategory = (catName) => {
+    fetch(`${api_url}/category/${catName}`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  };
+
+  useEffect(() => {
+    getProducts();
+    getCategories();
   }, []);
 
   return (
     <>
       <h2 className="text-center p-3">Our Products</h2>
       <div className="container">
+        <button
+          className="btn btn-info"
+          onClick={() => {
+            getProducts();
+          }}
+        >
+          All
+        </button>
+
+        {categories.map((cat) => {
+          return (
+            <button
+              className="btn btn-info"
+              key={cat}
+              onClick={() => {
+                getProductInCategory(cat);
+              }}
+            >
+              {cat}
+            </button>
+          );
+        })}
+
         <div className="row">
           {products.map((product) => {
             return (
